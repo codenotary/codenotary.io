599bd9a02b24c58cc4609d8a1706d8521af00e6a
Dennis <60224695+vchaindz@users.noreply.github.com>
2020-09-06 14:15:34 +0200

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,26 +2,21 @@
     <div>
    		<page-section>
             <page-section-header>
-                <h1 slot="title" class="d6">vChain, Inc.</h1>
+                <h1 slot="title" class="d6">Contact us</h1>
             </page-section-header>
             <i-container>
-                <i-row>
+                <i-row class="_align-items-center">
                     <i-column>
-					<h2 class="_font-weight-bold _margin-top-1">
-                    4141 Southwest Fwy #340
+					<h1 slot="title" class="d6">vChain, Inc.</h1>			
+     				<h2 class="_font-weight-bold _margin-top-1">
+                    4141 Southwest Fwy, Suite 340
 					</br>
                     Houston, TX 77027, United States
 					</h2>
                     </i-column>
                 </i-row>
             </i-container>
-        </page-section>
-
-        <page-section>
-            <page-section-header>
-                <h2 slot="title" class="d6">Contact us</h2>
-            </page-section-header>
-            <i-container>
+			<i-container>
                 <i-row>
                     <i-column>
                         <contact-form />
