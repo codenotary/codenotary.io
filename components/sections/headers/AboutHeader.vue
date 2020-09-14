d6b2c700e16fb6db7b32b44c9decb097ada695ff
Dennis <60224695+vchaindz@users.noreply.github.com>
2020-09-14 16:32:38 +0200

    Update AboutHeader.vue

--- components/sections/headers/AboutHeader.vue
+++ components/sections/headers/AboutHeader.vue
@@ -3,13 +3,13 @@
         <i-row class="_align-items-center">
             <i-column md="8">
                 <h1 class="d6 _font-weight-bold _text-primary _margin-top-0">
-                    vChain Inc
+                    We are vChain Inc
                 </h1>
                 <h2 class="_font-weight-bold _margin-top-1">
-                    is the leading company for immutable recording of data and processes.
+                    the leading company for immutable recording of data and processes.
                 </h2>
                 <p class="lead _margin-bottom-0">
-                    Our solutions can be used on-premise or in the cloud, are always easy to use and can process millions of transactions a second with cryptographic verification.
+                    Our solutions can be used on-premise or in the cloud, are easy to use and can process millions of transactions a second with cryptographic verification.
                 </p>
             </i-column>
             <i-column md="4" class="_text-center">
