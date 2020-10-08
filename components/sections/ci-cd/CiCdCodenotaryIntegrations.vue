a38998b55406fcda54ac5aa51849d95d0234acdd
Stefano Salidu <stefano@opvizor.com>
2020-10-08 10:47:44 +0200

    chore: updated CiCdCodenotaryIntegration section with new colors

--- components/sections/ci-cd/CiCdCodenotaryIntegrations.vue
+++ components/sections/ci-cd/CiCdCodenotaryIntegrations.vue
@@ -10,40 +10,40 @@
             </i-row>
             <i-row>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="immudb" subtitle="high-speed immutable database" variant="dark" image="/images/immudb/mascot.png" />
+                    <story-card title="immudb" subtitle="high-speed immutable database" :color="getColor(0)" image="/images/immudb/mascot.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Docker" subtitle="Docker container protection" variant="info" image="/images/integrations/white/docker.png" />
+                    <story-card title="Docker" subtitle="Docker container protection" :color="getColor(1)" image="/images/integrations/white/docker.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Kubernetes" subtitle="Kube-notary for Kubernetes" variant="blue" image="/images/integrations/white/kubernetes.png" />
+                    <story-card title="Kubernetes" subtitle="Kube-notary for Kubernetes" :color="getColor(2)" image="/images/integrations/white/kubernetes.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Javascript" subtitle="JSVCN library for the web" variant="warning" image="/images/integrations/white/javascript.png" />
+                    <story-card title="Javascript" subtitle="JSVCN library for the web" :color="getColor(3)" image="/images/integrations/white/javascript.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Java" subtitle="JVCN library for Java" variant="red" image="/images/integrations/white/java.png" />
+                    <story-card title="Java" subtitle="JVCN library for Java" :color="getColor(4)" image="/images/integrations/white/java.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Maven" subtitle="Java Maven package" variant="pink" image="/images/integrations/white/maven.png" />
+                    <story-card title="Maven" subtitle="Java Maven package" :color="getColor(5)" image="/images/integrations/white/maven.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Jenkins" subtitle="Jenkins CI" variant="red" image="/images/integrations/white/jenkins.png" />
+                    <story-card title="Jenkins" subtitle="Jenkins CI" :color="getColor(6)" image="/images/integrations/white/jenkins.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Powershell" subtitle="Windows command line" variant="teal" image="/images/integrations/white/powershell.png" />
+                    <story-card title="Powershell" subtitle="Windows command line" :color="getColor(7)" image="/images/integrations/white/powershell.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Github" subtitle="Github Actions" variant="dark" image="/images/integrations/white/github.png" />
+                    <story-card title="Github" subtitle="Github Actions" :color="getColor(8)" image="/images/integrations/white/github.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Gitlab" subtitle="Gitlab CI/CD" variant="purple" image="/images/integrations/white/gitlab.png" />
+                    <story-card title="Gitlab" subtitle="Gitlab CI/CD" :color="getColor(9)" image="/images/integrations/white/gitlab.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Git" subtitle="Standalone" variant="orange" image="/images/integrations/white/git.png" />
+                    <story-card title="Git" subtitle="Standalone" :color="getColor(10)" image="/images/integrations/white/git.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Travis" subtitle="Travis CI" variant="success" image="/images/integrations/white/travis.png" />
+                    <story-card title="Travis" subtitle="Travis CI" :color="getColor(11)" image="/images/integrations/white/travis.png" />
                 </i-column>
             </i-row>
         </i-container>
@@ -51,10 +51,14 @@
 </template>
 
 <script>
+import colorUtils from '~/mixins/colorUtils';
 import StoryCard from '~/components/common/cards/StoryCard';
 
 export default {
     name: 'CiCdCodenotaryIntegrations',
+
+    mixins: [colorUtils],
+
     components: {
         StoryCard
     }
