76104652fad77fd21e8c6a4c2eb7bbc077086072
Stefano Salidu <stefano@opvizor.com>
2020-10-21 20:20:47 +0200

    chore: updated immudb page

--- components/sections/headers/ImmudbHeader.vue
+++ components/sections/headers/ImmudbHeader.vue
@@ -5,6 +5,15 @@
         <img src="/images/immudb/mascot.png" alt="immudb mascot">
 
         <h1 class="d6 _text-primary">immudb</h1>
+
+        <div id="github-button">
+            <github-button href="https://github.com/codenotary/immudb"
+                           data-icon="octicon-star" data-size="large" data-show-count="true"
+                           aria-label="Star codenotary/immudb on GitHub">
+                Star
+            </github-button>
+        </div>
+
         <p class="description lead">
             immudb - the lightweight, high-speed immutable database for systems and applications. Open Source and easy to integrate into any existing application.
         </p>
@@ -14,27 +23,25 @@
         </p>
 
         <i-nav class="header-nav">
-            <i-nav-item href="https://docs.immudb.io">Documentation</i-nav-item>
+            <i-button id="research-paper-button underline" link variant="primary" @click="researchPaperModalVisible = true">
+                <span>Research Paper</span>
+            </i-button>
             <i-nav-item class="dot _hidden-sm-and-down">&middot;</i-nav-item>
-            <i-nav-item :to="{ name: 'technologies-immudb-immutable-data-science' }">Immutable Data Science</i-nav-item>
+            <i-nav-item class="underline" :to="{ name: 'technologies-immudb-immutable-data-science' }">Immutable Data Science</i-nav-item>
             <i-nav-item class="dot _hidden-sm-and-down">&middot;</i-nav-item>
-            <i-nav-item href="https://github.com/codenotary/immudb">GitHub</i-nav-item>
+            <i-nav-item class="underline" href="https://github.com/codenotary/immudb">GitHub</i-nav-item>
         </i-nav>
 
         <p class="_margin-top-3">
-            <i-button id="research-paper-button" link variant="primary" @click="researchPaperModalVisible = true">
-                <i-badge size="sm" variant="success" class="_margin-right-1-2">New</i-badge>
-                <span>Download Research Paper</span>
+            <i-button id="research-paper-button" class="underline" link variant="primary" @click="openCodingChallenge()">
+                <!-- <i-badge size="sm" variant="success" class="_margin-right-1-2">New</i-badge> -->
+                <i-badge size="sm" variant="danger" class="_margin-right-1-2">
+                    <fa icon="fire" style="margin-right: .15rem" />
+                    Hot
+                </i-badge>
+                <span>immudb Coding Challenge</span>
             </i-button>
         </p>
-
-        <div id="github-button">
-            <github-button href="https://github.com/codenotary/immudb"
-                           data-icon="octicon-star" data-size="large" data-show-count="true"
-                           aria-label="Star codenotary/immudb on GitHub">
-                Star
-            </github-button>
-        </div>
     </i-header>
 </template>
 
@@ -44,14 +51,23 @@ import ResearchPaperModal from '~/components/common/modals/ResearchPaperModal';
 
 export default {
     name: 'ImmudbHeader',
+
     components: {
         ResearchPaperModal,
         GithubButton
     },
+
     data() {
         return {
             researchPaperModalVisible: false
         };
+    },
+
+    methods: {
+        openCodingChallenge () {
+            const data = 'https://github.com/codenotary/immuchallenge';
+            data && window.open(data, '_blank');
+        }
     }
 }
 </script>
@@ -83,6 +99,10 @@ export default {
 
     .header-nav {
         justify-content: center;
+
+        .underline {
+            text-decoration: underline;
+        }
 
         .dot {
             color: $text-muted;
