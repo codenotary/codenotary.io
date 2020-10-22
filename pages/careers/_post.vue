b8f9c5e70a5d2b815904ad733c666a0f7a80d14d
Stefano Salidu <stefano@opvizor.com>
2020-10-22 10:25:56 +0200

    style: added career location, type and requires to career post page

--- pages/careers/_post.vue
+++ pages/careers/_post.vue
@@ -3,6 +3,15 @@
         <page-section-header :title="article.title" tag="h1" />
         <i-container>
             <i-row class="_display-flex _flex-direction-column">
+                <i-column v-if="article.location || article.type || article.requires" class="_margin-bottom-1" xs="12">
+                    <i-card>
+                        <p v-if="article.location || article.type" class="_margin-top-0 _margin-bottom-0">
+                            <i-badge v-if="article.location" class="_margin-top-1-4 _margin-right-1-4" size="sm">{{ article.location }}</i-badge>
+                            <i-badge v-if="article.type" class="_margin-top-1-4 _margin-right-1-4" size="sm">{{ article.type }}</i-badge>
+                        </p>
+                        <p v-if="article.requires" class="lead _margin-top-1 _margin-right-1-4 _margin-bottom-0" style="font-size: 80%">{{ article.requires }}</p>
+                    </i-card>
+                </i-column>
                 <i-column xs="12">
                     <article>
                         <nuxt-content :document="article" />
