8a2b134718d686e118e9389737270fd4cd3f5e9f
StefanoSalidu <st.salidu@gmail.com>
2020-11-09 17:11:09 +0100

    style: better aquisition banner

--- components/layout/Banner.vue
+++ components/layout/Banner.vue
@@ -1,12 +1,13 @@
 <template>
     <section class="banner _justify-content-center">
         <div class="_display-flex _align-items-center">
-            <span class="lead">
+            <fa icon="chart-line" class="_margin-top-1-4 _margin-right-1" />
+            <span class="lead _font-weight-bold">
                 vChain announces acquisition of Opvizor! Now offers CodeNotary for DevOps and Ops.
             </span>
         </div>
         <div class="_display-flex _align-items-center">
-            <i-button class="_margin-left-2" variant="dark" outline size="sm" :to="{ name: 'products-lc-for-ops' }">Learn more</i-button>
+            <i-button class="_margin-left-2 _font-weight-bold" variant="dark" outline size="sm" :to="{ name: 'products-ledger-compliance-metrics-and-logs' }">Learn more</i-button>
         </div>
     </section>
 </template>
@@ -24,14 +25,10 @@ export default {
 section.banner {
     background-color: $color-warning;
 
-    // &.-light {
-    //     border-bottom: 1px solid $border-color-light;
-    //     background: white !important;
-    // }
-
     span,
     .button {
         font-size: 90% !important;
+        border-color: rgba(0, 0, 0, 0.35);
         color: rgba(0, 0, 0, 0.7);
     }
 
@@ -39,16 +36,19 @@ section.banner {
         &:hover,
         &:active,
         &.nuxt-link-active {
+                border-color: rgba(0, 0, 0, 0.55) !important;
                 color: rgba(0, 0, 0, 0.9) !important;
-                // color: $color-gray-30 !important;
         }
 
         &:hover {
+            border-width: 2px !important;
             background: rgba(255, 255, 255, .15) !important;
         }
 
         &:active,
         &.nuxt-link-active {
+            border-width: 2px !important;
+            border-color: rgba(0, 0, 0, 0.55) !important;
             background: rgba(255, 255, 255, .05) !important;
         }
     }
