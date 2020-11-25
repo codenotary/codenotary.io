8776063e407a1a35138b2324a1d586df981b99e6
StefanoSalidu <st.salidu@gmail.com>
2020-11-25 11:17:57 +0100

    chore: added new Feature section in homepage section

---
+++ components/sections/homepage/FeaturesSection.vue
@@ -0,0 +1,132 @@
+<template>
+    <page-section id="features-section">
+        <i-container>
+            <i-row>
+                <i-column xs="12" lg="4">
+                    <Feature title="Immutable"
+                             image="/images/features/immutable.svg">
+                        <ul class="list -unstyled">
+                            <li>
+                                Any kind of key-value. Clients can choose how to structure data.
+                            </li>
+                            <li>
+                                <strong>No data mutation APIs</strong> are provided.
+                            </li>
+                            <li>
+                                Data is never overwritten, so multiple versions of the same key co-exist and are verifiable.
+                            </li>
+                        </ul>
+                    </Feature>
+                </i-column>
+                <i-column xs="12" lg="4">
+                    <Feature title="Auditable"
+                             image="/images/features/auditable.svg">
+                        <ul class="list -unstyled">
+                            <li>
+                                Tamper-evident history system.
+                            </li>
+                            <li>
+                                Clients and auditors want <strong>cryptographic proof</strong> of data inclusion and
+                                historical consistency in real time.
+                            </li>
+                            <li>If tampered, clients and auditors will notice that and take actions.</li>
+                        </ul>
+                    </Feature>
+                </i-column>
+                <i-column xs="12" lg="4">
+                    <Feature title="Secure"
+                             image="/images/features/secure.svg">
+                        <ul class="list -unstyled">
+                            <li>
+                                Data ownership is verifiable by clients and auditors.
+                            </li>
+                            <li>
+                                Sign your data using <strong>Public-Key Cryptography</strong>.
+                            </li>
+                            <li>
+                                Keys additions and revocations are immutably stored into the database.
+                            </li>
+                        </ul>
+                    </Feature>
+                </i-column>
+            </i-row>
+            <i-row v-if="false" class=" _margin-top-4">
+                <i-column xs="12" sm="6" lg="3">
+                    <Feature subtitle="Record data as it happens">
+                        <template v-slot:icon>
+                            <fa icon="database" class="fa-4x _text-primary-lightest _text-center"/>
+                        </template>
+                    </Feature>
+                </i-column>
+                <i-column xs="12" sm="6" lg="3">
+                    <Feature subtitle="Immediate data query">
+                        <template v-slot:icon>
+                            <fa icon="bolt" class="fa-4x _text-primary-lightest _text-center"/>
+                        </template>
+                    </Feature>
+                </i-column>
+                <i-column xs="12" sm="6" lg="3">
+                    <Feature subtitle="Cryptographic verification built-in">
+                        <template v-slot:icon>
+                            <fa icon="lock" class="fa-4x _text-primary-lightest _text-center"/>
+                        </template>
+                    </Feature>
+                </i-column>
+                <i-column xs="12" sm="6" lg="3">
+                    <Feature subtitle="All is online, not archived and out of reach">
+                        <template v-slot:icon>
+                            <fa icon="server" class="fa-4x _text-secondary _text-center"/>
+                        </template>
+                    </Feature>
+                </i-column>
+            </i-row>
+            <i-row class=" _margin-top-4">
+                <i-column class="_text-center _text-muted">
+                    <ul class="list -unstyled">
+                        <li class="_margin-bottom-2">
+                            <strong>Immutability</strong> by perimeter defense is proven not to work, <strong>Immutability</strong> needs to be embedded in the data structure!
+                        </li>
+                        <li>
+                            Powered by
+                            <a href="https://immudb.io">
+                                <img width="40" src="/images/logo/icon/logo_icon_black.svg" alt="immudb">
+                                <strong class="_text-black">immudb</strong>
+                            </a>
+                        </li>
+                    </ul>
+                </i-column>
+            </i-row>
+        </i-container>
+    </page-section>
+</template>
+
+<script>
+import Feature from '~/components/common/Feature';
+
+export default {
+    components: {
+        Feature
+    }
+}
+</script>
+
+<style lang="scss">
+@import "~@inkline/inkline/src/css/mixins";
+@import "~@inkline/inkline/src/css/config";
+
+#features-section {
+    background-color: #e9ecef;
+
+    .column {
+        @include breakpoint-down(lg) {
+            .feature {
+                margin-bottom: $spacer * 2;
+            }
+
+            &:last-child .feature {
+                margin-bottom: 0;
+            }
+        }
+    }
+}
+</style>
