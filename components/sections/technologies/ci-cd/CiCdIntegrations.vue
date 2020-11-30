1fe2ba7468accd2bdffc386a60bcf48910a79676
StefanoSalidu <st.salidu@gmail.com>
2020-11-30 22:47:08 +0100

    chore: udpated immudb integrations page

--- components/sections/technologies/ci-cd/CiCdIntegrations.vue
+++ components/sections/technologies/ci-cd/CiCdIntegrations.vue
@@ -10,40 +10,40 @@
             </i-row>
             <i-row>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="immudb" subtitle="high-speed immutable database" :color="getColor(0)" image="/images/immudb/mascot.png" />
+                    <story-card title="immudb" subtitle="high-speed immutable database" color="transparent" image="/images/immudb/mascot.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Docker" subtitle="Docker container protection" :color="getColor(1)" image="/images/integrations/white/square/docker.png" />
+                    <story-card title="Docker" subtitle="Docker container protection" color="transparent" image="/images/integrations/square/docker.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Kubernetes" subtitle="Kube-notary for Kubernetes" :color="getColor(2)" image="/images/integrations/white/square/kubernetes.png" />
+                    <story-card title="Kubernetes" subtitle="Kube-notary for Kubernetes" color="transparent" image="/images/integrations/square/kubernetes.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Javascript" subtitle="JSVCN library for the web" :color="getColor(3)" image="/images/integrations/white/square/javascript.png" />
+                    <story-card title="Javascript" subtitle="JSVCN library for the web" color="transparent" image="/images/integrations/square/javascript.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Java" subtitle="JVCN library for Java" :color="getColor(4)" image="/images/integrations/white/square/java.png" />
+                    <story-card title="Java" subtitle="JVCN library for Java" color="transparent" image="/images/integrations/square/java.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Maven" subtitle="Java Maven package" :color="getColor(5)" image="/images/integrations/white/square/maven.png" />
+                    <story-card title="Maven" subtitle="Java Maven package" color="transparent" image="/images/integrations/square/maven.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Jenkins" subtitle="Jenkins CI" :color="getColor(6)" image="/images/integrations/white/square/jenkins.png" />
+                    <story-card title="Jenkins" subtitle="Jenkins CI" color="transparent" image="/images/integrations/square/jenkins.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Powershell" subtitle="Windows command line" :color="getColor(7)" image="/images/integrations/white/square/powershell.png" />
+                    <story-card title="Powershell" subtitle="Windows command line" color="transparent" image="/images/integrations/square/powershell.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Github" subtitle="Github Actions" :color="getColor(8)" image="/images/integrations/white/square/github.png" />
+                    <story-card title="Github" subtitle="Github Actions" color="transparent" image="/images/integrations/square/github.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Gitlab" subtitle="Gitlab CI/CD" :color="getColor(9)" image="/images/integrations/white/square/gitlab.png" />
+                    <story-card title="Gitlab" subtitle="Gitlab CI/CD" color="transparent" image="/images/integrations/square/gitlab.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Git" subtitle="Standalone" :color="getColor(10)" image="/images/integrations/white/square/git.png" />
+                    <story-card title="Git" subtitle="Standalone" color="transparent" image="/images/integrations/square/git.png" />
                 </i-column>
                 <i-column xs="6" sm="4" md="3" lg="3">
-                    <story-card title="Travis" subtitle="Travis CI" :color="getColor(11)" image="/images/integrations/white/square/travis.png" />
+                    <story-card title="Travis" subtitle="Travis CI" color="transparent" image="/images/integrations/square/travis.png" />
                 </i-column>
             </i-row>
         </i-container>
@@ -80,7 +80,22 @@ export default {
         padding: 1rem;
 
         img {
-            height: 64px;
+            height: 84px;
+            // opacity: 0.55;
+        }
+
+        .title {
+            color: #333;
+        }
+
+        .subtitle {
+            color: #444;
+        }
+
+        &:hover {
+            img {
+                opacity: 1;
+            }
         }
 
         @include breakpoint-down(xs) {
