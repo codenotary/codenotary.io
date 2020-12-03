128389594234a9652f6763be4883096ab49c92ad
StefanoSalidu <st.salidu@gmail.com>
2020-12-03 16:50:41 +0100

    style: better blog page UI, in particular mobile viewport

--- pages/blog/_post.vue
+++ pages/blog/_post.vue
@@ -1,15 +1,15 @@
 <template>
-    <page-section>
+    <page-section class="article-section">
         <page-section-header :title="article.title" tag="h1" />
         <i-container>
-            <i-row class="_justify-content-space-between">
+            <i-row>
                 <i-column xs="12" sm="12" md="9">
-                    <article class="blog-post-page _padding-right-xs-0 _padding-right-sm-4">
+                    <article class="blog-post-page">
                         <nuxt-content :document="article" />
                     </article>
                 </i-column>
                 <i-column xs="12" sm="12" md="3">
-                    <most-popular class="most-popular-wrapper _padding-left-xs-1" :articles="mostPopular" card />
+                    <most-popular class="most-popular-wrapper" :articles="mostPopular" card />
                 </i-column>
             </i-row>
         </i-container>
@@ -17,13 +17,16 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 import { title, meta, DEFAULT_META } from '@/helpers/meta/index';
 import MostPopular from '~/components/global/MostPopular';
+import { VIEW_MODULE, MOBILE } from '@/store/view/constants';
 
 export default {
     components: {
         MostPopular
     },
+
     head () {
         return {
             title: title(this.article.title),
@@ -42,6 +45,7 @@ export default {
             ]
         };
     },
+
     async asyncData({ $content, params }) {
         const BLOG_POST_PATH = 'blog';
         const article = await $content(BLOG_POST_PATH, params.post).fetch();
@@ -49,18 +53,11 @@ export default {
 
         return { article, mostPopular };
     },
-    methods: {
-        jsonTreeToString (data) {
-            if (data && data.length) {
-                return data.reduce((acc, x, idx) => {
-                    const result = x && x.children ? this.jsonTreeToString(x.children) : x.value;
-                    result && result !== 'undefined' && (acc += result);
-                    return acc;
-                }, '');
-            }
-        }
-    },
+
     computed: {
+        ...mapGetters(VIEW_MODULE, {
+            mobile: MOBILE
+        }),
         getDescription () {
             const MAX_DESCRIPTION = 160;
             const description = this.jsonTreeToString(this.article.body.children)
@@ -78,6 +75,18 @@ export default {
         postUrl () {
             return `${DEFAULT_META.SITE_URL}${this.$route.fullPath}`;
         }
+    },
+
+    methods: {
+        jsonTreeToString (data) {
+            if (data && data.length) {
+                return data.reduce((acc, x, idx) => {
+                    const result = x && x.children ? this.jsonTreeToString(x.children) : x.value;
+                    result && result !== 'undefined' && (acc += result);
+                    return acc;
+                }, '');
+            }
+        }
     }
 }
 </script>
@@ -86,17 +95,90 @@ export default {
 @import "~@inkline/inkline/src/css/mixins";
 @import "~@inkline/inkline/src/css/config";
 
-article.blog-post-page {
-    h2 {
-        margin-top: 0 !important;
+section.article-section {
+    article.blog-post-page {
+        padding-right: $spacer-4;
+
+        img {
+            display:block;
+            max-width: 100%;
+            margin: $spacer-2 auto;
+            padding: 0 0 $spacer-1 $spacer-1;
+        }
+
+        table {
+            width: 100%;
+            margin: $spacer-2 0;
+            border-collapse: collapse;
+        }
+
+        tr:nth-of-type(even) {
+            background: lighten($color-primary, 60%);
+        }
+
+        th {
+            background: $color-primary;
+            color: $color-white;
+            font-weight: bold;
+        }
+
+        td, th {
+            padding: 6px;
+            border: 1px solid #e6e6e6;
+            text-align: left;
+        }
+
+        .nuxt-content-highlight {
+            margin: $spacer-1-2 0;
+        }
+
+        @include breakpoint-up(md) {
+            border-right: 1px solid #e6e6e6;
+        }
+
+        @include breakpoint-down(sm) {
+            padding: 0;
+            padding-bottom: $spacer-2;
+
+            h1 {
+                font-size: 1.5rem;
+                font-weight: bold;
+            }
+
+            h2 {
+                font-size: 1.25rem;
+                font-weight: bold;
+            }
+
+            img {
+                width: 100%;
+                padding: 0;
+            }
+
+            table {
+                table-layout: fixed;
+                width: 100%;
+                word-wrap: break-word;
+            }
+        }
     }
 
-    img {
-        max-width: 95%;
+    .most-popular-wrapper {
+        padding: 0;
+
+        @include breakpoint-down(sm) {
+            padding-top: $spacer-2;
+            padding-left: $spacer-1;
+            border-top: 1px solid #e6e6e6;
+        }
     }
 
-    @include breakpoint-up(sm) {
-        border-right: 1px solid #e6e6e6;
+    @include breakpoint-down(sm) {
+        padding: 0 !important;
+
+        .section-header {
+            margin-bottom: $spacer-1 !important;
+        }
     }
 }
 </style>
