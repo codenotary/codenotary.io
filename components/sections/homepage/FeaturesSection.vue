d678b9280a79283f25332e0b21691554083020a6
Dennis <60224695+vchaindz@users.noreply.github.com>
2020-12-20 15:40:51 +0100

    Update FeaturesSection.vue

--- components/sections/homepage/FeaturesSection.vue
+++ components/sections/homepage/FeaturesSection.vue
@@ -14,7 +14,7 @@
                             <li>
                                 Data is never overwritten and is cryptographically verifiable.
                             </li>
-                            <li>Adress regulatory compliance: SEC 17a-4(f), CFTC 1.31(d), FINRA, and other regulations.</li>
+                            <li>Address regulatory compliance: SEC 17a-4(f), CFTC 1.31(d), FINRA, and other regulations.</li>
                         </ul>
                     </Feature>
                 </i-column>
