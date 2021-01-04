4f476e01a7d75e302da009b0091cfeac63e82e9f
StefanoSalidu <st.salidu@gmail.com>
2021-01-04 12:06:35 +0100

    chore: added new eslint configurations (the same used in the ledger-compliance project)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,63 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  // add your custom rules here
-  rules: {
-      indent: ["warn",4],
-      "vue/html-indent": ["warn",4],
-      "arrow-parens": ["warn", "always"],
-      "vue/html-closing-bracket-spacing": "off",
-      'semi': 'off',
-      "no-multiple-empty-lines": 'off',
-      "vue/order-in-components": 'off',
-      "vue/html-closing-bracket-newline": 'off',
-      "vue/singleline-html-element-content-newline": 'off',
-      "vue/max-attributes-per-line": 'off',
-      "space-before-function-paren": 'off'
-  }
+	globals: {
+		XPay: true,
+	},
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		parser: 'babel-eslint',
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended',
+	],
+	// add your custom rules here
+	rules: {
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		indent: ['warn', 'tab', {
+			SwitchCase: 1,
+			MemberExpression: 2,
+		}],
+		'space-before-function-paren': 'off',
+		'no-tabs': 'off',
+		'brace-style': ['error', 'stroustrup'],
+		'comma-dangle': [
+			'warn', {
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'always-multiline',
+			},
+		],
+		'vue/no-v-html': 'off',
+		'vue/html-indent': ['warn', 'tab', {
+			baseIndent: 1,
+			attribute: 1,
+		}],
+		'vue/max-attributes-per-line': ['warn', {
+			singleline: 2,
+			multiline: {
+				max: 2,
+			},
+			allowFirstLine: false,
+		}],
+		'vue/html-closing-bracket-newline': ['warn', {
+			multiline: 'always',
+			singleline: 'never',
+		}],
+		'template-curly-spacing': ['error', 'always'],
+		'no-console': 'off', // TODO: turn on
+		'no-debugger': 'off',
+		'vue/require-component-is': 'off',
+		'spaced-comment': 'warn',
+		'vue/no-unused-vars': 'warn',
+		'no-trailing-spaces': 'warn',
+		'no-multiple-empty-lines': 'warn',
+		'padded-blocks': 'warn',
+	},
 };
--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,34 @@
 module.exports = {
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
-  },
-  moduleFileExtensions: [
-    'js',
-    'vue',
-    'json'
-  ],
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
-  },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
-  ]
-}
+	preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/$1',
+		'^~/(.*)$': '<rootDir>/$1',
+		'^vue$': 'vue/dist/vue.common.js',
+	},
+
+	moduleFileExtensions: [
+		'js',
+		'vue',
+		'json',
+	],
+
+	transform: {
+		'^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+		'.*\\.(vue)$': '<rootDir>/node_modules/vue-jest',
+		'.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': '<rootDir>/node_modules/jest-transform-stub',
+	},
+	transformIgnorePatterns: ['/node_modules/(?!@inkline).\\.js$'],
+
+	snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
+
+	collectCoverage: true,
+	collectCoverageFrom: [
+		'<rootDir>/components/**/*.vue',
+		'<rootDir>/pages/**/*.vue',
+		'<rootDir>/helpers/**/*.js',
+		'<rootDir>/middleware/**/*.js',
+		'<rootDir>/services/**/*.js',
+		// '<rootDir>/store/**/*.js'
+	],
+	setupFiles: ['jest-canvas-mock'],
+};
