00a94635a90e64d9436e19bc9a3f3467e310efb4
StefanoSalidu <st.salidu@gmail.com>
2021-01-04 12:06:49 +0100

    chore: linted nuxt file

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,263 +4,263 @@ import careersRoutes from './careers';
 import tosRoutes from './tos';
 
 export default {
-    /*
+	/*
     ** ssr propery
     ** Doc: https://nuxtjs.org/guides/configuration-glossary/configuration-ssr
     */
-    ssr: true,
-    /*
+	ssr: true,
+	/*
     ** components propery
     ** Doc: https://nuxtjs.org/api/configuration-components/
     */
-    components: true,
-    /*
+	components: true,
+	/*
     ** Headers of the page
     */
-    head: {
-        htmlAttrs: { lang: 'en-GB' },
-        title: DEFAULT_META.TITLE,
-        meta: [
-            ...meta(),
-            { charset: 'utf-8' },
-            { name: "HandheldFriendly", content: "True" },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            // Open Graph Data
-            { property: 'og:site_name', content: DEFAULT_META.SITE_NAME },
-            // Twitter Card
-            { name: 'twitter:site', content: DEFAULT_META.HANDLE },
-            { name: 'twitter:card', content: 'summary_large_image' },
-        ],
-        link: [
-            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'manifest', href: '/site.webmanifest' },
-            { hid: 'canonical', rel: 'canonical', href: DEFAULT_META.SITE_URL }
-        ]
-    },
-    /*
+	head: {
+		htmlAttrs: { lang: 'en-GB' },
+		title: DEFAULT_META.TITLE,
+		meta: [
+			...meta(),
+			{ charset: 'utf-8' },
+			{ name: 'HandheldFriendly', content: 'True' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			// Open Graph Data
+			{ property: 'og:site_name', content: DEFAULT_META.SITE_NAME },
+			// Twitter Card
+			{ name: 'twitter:site', content: DEFAULT_META.HANDLE },
+			{ name: 'twitter:card', content: 'summary_large_image' },
+		],
+		link: [
+			{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+			{ rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+			{ rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'manifest', href: '/site.webmanifest' },
+			{ hid: 'canonical', rel: 'canonical', href: DEFAULT_META.SITE_URL },
+		],
+	},
+	/*
     ** Customize the progress-bar color
     */
-    loading: { color: '#fff' },
-    /*
+	loading: { color: '#fff' },
+	/*
     ** Global CSS
     */
-    css: [
-        'vue-slick-carousel/dist/vue-slick-carousel.css',
-        'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
-    ],
-    /*
+	css: [
+		'vue-slick-carousel/dist/vue-slick-carousel.css',
+		'vue-slick-carousel/dist/vue-slick-carousel-theme.css',
+	],
+	/*
     ** Plugins to load before mounting the App
     */
-    plugins: [
-        { src: '~plugins/activecampaign', mode: 'client' },
-        { src: '~plugins/vgo', mode: 'client' },
-        { src: '~plugins/leadfeeder', mode: 'client' },
-        { src: '~plugins/tawk', mode: 'client' },
-        { src: '~plugins/vue-cool-lightbox', mode: 'client' },
-        { src: '~plugins/prism', ssr: false  },
-        { src: '~plugins/vue-masonry-css', ssr: false },
-        { src: '~plugins/vue-truncate-filter', ssr: false },
-        { src: '~plugins/vue-typer', ssr: false },
-        '~plugins/vue-slick-carousel',
-        '~plugins/inkline'
-    ],
-    /*
+	plugins: [
+		{ src: '~plugins/activecampaign', mode: 'client' },
+		{ src: '~plugins/vgo', mode: 'client' },
+		{ src: '~plugins/leadfeeder', mode: 'client' },
+		{ src: '~plugins/tawk', mode: 'client' },
+		{ src: '~plugins/vue-cool-lightbox', mode: 'client' },
+		{ src: '~plugins/prism', ssr: false },
+		{ src: '~plugins/vue-masonry-css', ssr: false },
+		{ src: '~plugins/vue-truncate-filter', ssr: false },
+		{ src: '~plugins/vue-typer', ssr: false },
+		'~plugins/vue-slick-carousel',
+		'~plugins/inkline',
+	],
+	/*
     ** Nuxt.js dev-modules
     */
-    buildModules: [
-        // Doc: https://github.com/nuxt-community/eslint-module
-        '@nuxtjs/eslint-module',
-        // Doc: https://github.com/nuxt-community/stylelint-module
-        '@nuxtjs/stylelint-module',
-        // Doc: https://github.com/nuxt-community/analytics-module
-        ['@nuxtjs/google-analytics', {
-            id: 'UA-136167888-1'
-        }]
-    ],
-    /*
+	buildModules: [
+		// Doc: https://github.com/nuxt-community/eslint-module
+		'@nuxtjs/eslint-module',
+		// Doc: https://github.com/nuxt-community/stylelint-module
+		'@nuxtjs/stylelint-module',
+		// Doc: https://github.com/nuxt-community/analytics-module
+		['@nuxtjs/google-analytics', {
+			id: 'UA-136167888-1',
+		}],
+	],
+	/*
     ** Nuxt.js modules
     */
-    modules: [
-        // Doc: https://content.nuxtjs.org/
-        '@nuxt/content',
-        // Doc: https://github.com/nuxt-community/style-resources-module
-        '@nuxtjs/style-resources',
-        // Doc: https://github.com/rigor789/vue-scrollto
-        'vue-scrollto/nuxt',
-        // Doc: https://www.npmjs.com/package/@nuxtjs/device
-        '@nuxtjs/device',
-        // https://www.npmjs.com/package/cookie-universal-nuxt
-        'cookie-universal-nuxt',
-        // Doc: https://github.com/vaso2/nuxt-fontawesome
-        ['nuxt-fontawesome', {
-            component: 'fa',
-            imports: [
-                {
-                    set: '@fortawesome/free-solid-svg-icons',
-                    icons: [
-                        'faCheckSquare',
-                        'faCheckCircle',
-                        'faTimesCircle',
-                        'faStar',
-                        'faServer',
-                        'faDatabase',
-                        'faBolt',
-                        'faLock',
-                        'faChevronRight',
-                        'faQuoteLeft',
-                        'faFire',
-                        'faGlobe',
-                        'faAward',
-                        'faChartLine'
-                    ]
-                },
-                {
-                    set: '@fortawesome/free-brands-svg-icons',
-                    icons: ['faFacebookSquare', 'faTwitterSquare', 'faLinkedin', 'faGithubSquare']
-                }
-            ]
-        }],
-        // Doc: https://github.com/Developmint/nuxt-webfontloader
-        'nuxt-webfontloader',
-        // Doc: https://github.com/nuxt-community/robots-module
-        '@nuxtjs/robots',
-        // Doc: https://github.com/nuxt-community/sitemap-module
-        '@nuxtjs/sitemap'
-    ],
+	modules: [
+		// Doc: https://content.nuxtjs.org/
+		'@nuxt/content',
+		// Doc: https://github.com/nuxt-community/style-resources-module
+		'@nuxtjs/style-resources',
+		// Doc: https://github.com/rigor789/vue-scrollto
+		'vue-scrollto/nuxt',
+		// Doc: https://www.npmjs.com/package/@nuxtjs/device
+		'@nuxtjs/device',
+		// https://www.npmjs.com/package/cookie-universal-nuxt
+		'cookie-universal-nuxt',
+		// Doc: https://github.com/vaso2/nuxt-fontawesome
+		['nuxt-fontawesome', {
+			component: 'fa',
+			imports: [
+				{
+					set: '@fortawesome/free-solid-svg-icons',
+					icons: [
+						'faCheckSquare',
+						'faCheckCircle',
+						'faTimesCircle',
+						'faStar',
+						'faServer',
+						'faDatabase',
+						'faBolt',
+						'faLock',
+						'faChevronRight',
+						'faQuoteLeft',
+						'faFire',
+						'faGlobe',
+						'faAward',
+						'faChartLine',
+					],
+				},
+				{
+					set: '@fortawesome/free-brands-svg-icons',
+					icons: ['faFacebookSquare', 'faTwitterSquare', 'faLinkedin', 'faGithubSquare'],
+				},
+			],
+		}],
+		// Doc: https://github.com/Developmint/nuxt-webfontloader
+		'nuxt-webfontloader',
+		// Doc: https://github.com/nuxt-community/robots-module
+		'@nuxtjs/robots',
+		// Doc: https://github.com/nuxt-community/sitemap-module
+		'@nuxtjs/sitemap',
+	],
 
-    /*
+	/*
     ** Build configuration
     */
 
-    content: {
-        dir: 'pages',
-        markdown: {
-            prism: {
-                theme: 'prism-themes/themes/prism-dracula.css'
-            }
-        }
-    },
+	content: {
+		dir: 'pages',
+		markdown: {
+			prism: {
+				theme: 'prism-themes/themes/prism-dracula.css',
+			},
+		},
+	},
 
-    styleResources: {
-        scss: ['~/assets/variables.scss']
-    },
+	styleResources: {
+		scss: ['~/assets/variables.scss'],
+	},
 
-    webfontloader: {
-        google: {
-            // Loads Open Sans font with weights 300 and 400 + display font as swap
-            families: ['Open+Sans:100,300,400,500,600,700&display=swap']
-        }
-    },
+	webfontloader: {
+		google: {
+			// Loads Open Sans font with weights 300 and 400 + display font as swap
+			families: ['Open+Sans:100,300,400,500,600,700&display=swap'],
+		},
+	},
 
-    build: {
-        transpile: [
-            '@inkline/inkline',
-            'vue-github-button'
-        ],
-        filenames: {
-            app: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js',
-            chunk: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js'
-        },
-        babel: {
-            plugins: [
-              '@babel/plugin-proposal-optional-chaining'
-            ]
-        }
-    },
+	build: {
+		transpile: [
+			'@inkline/inkline',
+			'vue-github-button',
+		],
+		filenames: {
+			app: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js',
+			chunk: ({ isDev }) => isDev ? '[name].[hash].js' : '[chunkhash].js',
+		},
+		babel: {
+			plugins: [
+				'@babel/plugin-proposal-optional-chaining',
+			],
+		},
+	},
 
-    router: {
-        linkExactActiveClass: '-active',
-        // base: '/'
-    },
+	router: {
+		linkExactActiveClass: '-active',
+		// base: '/'
+	},
 
-    generate: {
-        routes: () => {
-            let paths = [];
-            if (blogRoutes) {
-                const map = Object.keys(blogRoutes);
-                if (map && map.length) {
-                    map.map((path) => {
-                        paths = [...paths, {
-                            route: `/blog/${blogRoutes[path]}`,
-                            payload: path
-                        }]
-                    });                    
-                }
-            }
-            if (careersRoutes) {
-                const map = Object.keys(careersRoutes);
-                if (map && map.length) {
-                    map.map((path) =>
-                        paths = [...paths, {
-                            route: `/careers/${careersRoutes[path]}`,
-                            payload: path
-                        }
-                    ]);                    
-                }
-            }
-            if (tosRoutes) {
-                const map = Object.keys(tosRoutes);
-                if (map && map.length) {
-                    map.map((path) =>
-                        paths = [...paths, {
-                            route: `/terms-of-service/${tosRoutes[path]}`,
-                            payload: path
-                        }
-                    ]);                    
-                }
-            }
-            return paths;
-        }
-    },
+	generate: {
+		routes: () => {
+			let paths = [];
+			if (blogRoutes) {
+				const map = Object.keys(blogRoutes);
+				if (map && map.length) {
+					map.map((path) => {
+						paths = [...paths, {
+							route: `/blog/${ blogRoutes[path] }`,
+							payload: path,
+						}];
+					});
+				}
+			}
+			if (careersRoutes) {
+				const map = Object.keys(careersRoutes);
+				if (map && map.length) {
+					map.map((path) => {
+						paths = [...paths, {
+							route: `/careers/${ careersRoutes[path] }`,
+							payload: path,
+						}];
+					});
+				}
+			}
+			if (tosRoutes) {
+				const map = Object.keys(tosRoutes);
+				if (map && map.length) {
+					map.map((path) => {
+						paths = [...paths, {
+							route: `/terms-of-service/${ tosRoutes[path] }`,
+							payload: path,
+						}];
+					});
+				}
+			}
+			return paths;
+		},
+	},
 
-    sitemap: {
-        hostname: DEFAULT_META.SITE_URL,
-        gzip: true,
-        path: '/sitemap.xml',
-        routes: () => {
-            let paths = [];
-            if (blogRoutes) {
-                const map = Object.keys(blogRoutes);
-                if (map && map.length) {
-                    map.map((path) =>
-                        paths = [...paths, {
-                            route: `/blog/${blogRoutes[path]}`,
-                            payload: path
-                        }
-                    ]);                    
-                }
-            }
-            if (careersRoutes) {
-                const map = Object.keys(careersRoutes);
-                if (map && map.length) {
-                    map.map((path) =>
-                        paths = [...paths, {
-                            route: `/careers/${careersRoutes[path]}`,
-                            payload: path
-                        }
-                    ]);                    
-                }
-            }
-            if (tosRoutes) {
-                const map = Object.keys(tosRoutes);
-                if (map && map.length) {
-                    map.map((path) =>
-                        paths = [...paths, {
-                            route: `/terms-of-service/${tosRoutes[path]}`,
-                            payload: path
-                        }
-                    ]);                    
-                }
-            }
-            return paths;
-        },
-        defaults: {
-            changefreq: 'daily',
-            priority: 1,
-            lastmod: new Date()
-        }
-    }
-}
+	sitemap: {
+		hostname: DEFAULT_META.SITE_URL,
+		gzip: true,
+		path: '/sitemap.xml',
+		routes: () => {
+			let paths = [];
+			if (blogRoutes) {
+				const map = Object.keys(blogRoutes);
+				if (map && map.length) {
+					map.map((path) => {
+						paths = [...paths, {
+							route: `/blog/${ blogRoutes[path] }`,
+							payload: path,
+						}];
+					});
+				}
+			}
+			if (careersRoutes) {
+				const map = Object.keys(careersRoutes);
+				if (map && map.length) {
+					map.map((path) => {
+						paths = [...paths, {
+							route: `/careers/${ careersRoutes[path] }`,
+							payload: path,
+						}];
+					});
+				}
+			}
+			if (tosRoutes) {
+				const map = Object.keys(tosRoutes);
+				if (map && map.length) {
+					map.map((path) => {
+						paths = [...paths, {
+							route: `/terms-of-service/${ tosRoutes[path] }`,
+							payload: path,
+						}];
+					});
+				}
+			}
+			return paths;
+		},
+		defaults: {
+			changefreq: 'daily',
+			priority: 1,
+			lastmod: new Date(),
+		},
+	},
+};
