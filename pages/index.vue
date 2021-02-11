149ce43d41a4c2dc5a2448705b5e4517506914b8
StefanoSalidu <st.salidu@gmail.com>
2021-02-11 17:38:19 +0100

    chore: testing vue-lazy-hydrate when-visible on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,26 @@
 <template>
 	<div>
 		<HomeHeader />
-		<FeaturesSection />
-		<TimestampServiceSection />
-		<CustomerSuccessStories />
+		<LazyHydrate when-visible>
+			<FeaturesSection />
+		</LazyHydrate>
+		<LazyHydrate when-visible>
+			<TimestampServiceSection />
+		</LazyHydrate>
+		<LazyHydrate when-visible>
+			<CustomerSuccessStories />
+		</LazyHydrate>
 	</div>
 </template>
 
 <script>
+import LazyHydrate from 'vue-lazy-hydration';
+
 export default {
 	name: 'Homepage',
 	layout: 'banner',
+	components: {
+		LazyHydrate,
+	},
 };
 </script>
