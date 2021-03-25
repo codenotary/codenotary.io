09a4cf8afda853ea36327b91e9e43c07d0d02673
Artem Martynyuk <scarg.dev@gmail.com>
2021-03-25 16:10:01 +0100

    feat(CnButton): add generic button as a wrapper for underlying component libraries in order to support any future standardization of the buttons

---
+++ components/global/CnButton.vue
@@ -0,0 +1,46 @@
+<template>
+	<i-button
+		:size="size"
+		:outline="outline"
+		:variant="variant"
+		:href="href"
+		:target="href ? '_blank' : null"
+		:rel="href ? 'nofollow' : null"
+		:to="to"
+		@click="$emit('click', $event)"
+	>
+		<slot />
+	</i-button>
+</template>
+<script>
+export default {
+	name: 'CnButton',
+	props: {
+		type: {
+			type: String,
+			default: 'dark',
+		},
+		href: {
+			type: String,
+			default: null,
+		},
+		outline: {
+			type: Boolean,
+		},
+		to: {
+			type: Object,
+			default: null,
+		},
+		size: {
+			type: String,
+			default: 'lg',
+		},
+	},
+	computed: {
+		variant() {
+			return this.type;
+			// return 'dark';
+		},
+	},
+};
+</script>
