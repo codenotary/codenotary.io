cef7a3c3ecb6d7de7a93ec6205eb83811b471830
Artem Martynyuk <scarg.dev@gmail.com>
2021-03-29 15:59:39 +0200

    refactor(HighlightedCards): remove unnecessary tag and add image-url tag placeholder

--- components/sections/homepage/HighlightedCards.vue
+++ components/sections/homepage/HighlightedCards.vue
@@ -1,8 +1,9 @@
 <template>
-	<i-container :fluid="false" class="highlighted-cards">
+	<i-container class="highlighted-cards">
 		<highlighted-card
 			title="Trusted CI/CD"
 			subtitle="Add to any DevOps process within minutes"
+			image-url="/images/placeholder.svg"
 		>
 			<cn-button slot="action" link variant="dark" href="https://www.youtube.com/watch?v=jNyAWlBfTTs">
 				WATCH VIDEO
@@ -12,6 +13,7 @@
 		<highlighted-card
 			title="Tutorial"
 			subtitle="Getting Started with immudb in GO"
+			image-url="/images/placeholder.svg"
 		>
 			<cn-button slot="action" link variant="dark" :to="{ name: 'blog-post', params: { post: 'immudb-go-getting-started-video' }}">
 				READ BLOG
@@ -21,6 +23,7 @@
 		<highlighted-card
 			title="Trusted Machine Data"
 			subtitle="Metrics, Telemetry and logs analytics"
+			image-url="/images/placeholder.svg"
 		>
 			<cn-button slot="action" link variant="dark" :to="{ name: 'blog-post', params: { post: 'codenotary-timestamp-service-for-devs' }}">
 				READ BLOG
