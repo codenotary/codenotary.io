f5ce22bc70def83899c68102bb616ac89ad78788
Artem Martynyuk <scarg.dev@gmail.com>
2021-04-13 16:39:00 +0200

    feat(BenefitsSection): update page according to latest mockup

--- components/sections/homepage/BenefitsSection.old.vue
+++ components/sections/homepage/BenefitsSection.old.vue
@@ -6,7 +6,7 @@
 		<i-container class="_display-flex _flex-direction-column _align-items-center _justify-content-center _margin-bottom-6">
 			<hr class="_width-100"/>
 			<PageSectionHeader
-				:title="content.benefitsSection.title"
+				title="Key benefits of using CodeNotary Ledger Compliance"
 				:top="0"
 				:bottom="0"
 			/>
@@ -23,7 +23,29 @@
 				<div v-for="(slide, slideIndex) in content.benefitsSection.slides" :key="slideIndex"
 					class="slide"
 				>
+					<div class="imageLabel">
+						{{ slide.imageLabel }}
+					</div>
 					<img :src="slide.imageUrl" draggable="false">
+					<div class="details _margin-left-1">
+						<h5 class="title _font-weight-bold">
+							{{ slide.title }}
+						</h5>
+						<div class="content cn-text-muted_blue">
+							<ul>
+								<li v-for="(feature, featureIndex) in slide.list" :key="featureIndex">
+									{{ feature }}
+								</li>
+							</ul>
+						</div>
+
+						<cn-button link variant="dark"
+							class="action cn-text-muted_blue"
+							:to="slide.internalLink ? { name: slide.internalLink.type, params: { post: slide.internalLink.entry }} : null" :href="slide.href">
+							{{ slide.actionText }}
+							<fa class="icon _margin-left-2" icon="arrow-right" />
+						</cn-button>
+					</div>
 				</div>
 			</VueSlickCarousel>
 		</i-container>
@@ -58,28 +80,63 @@ export default {
 <style lang="scss" scoped>
 
 .custom-arrow {
-	color: $cn-color-dark;
+	color: #a1aec0;
 }
 
 .slide {
 	position: relative;
-	border-radius: 7px !important;
-	//border: 3px solid $cn-color-grey;
+	border-radius: 30px;
+	border: 3px solid #a1aec0;
 	overflow: hidden;
-	//height: 500px;
+	height: 500px;
 	display: flex !important;
 	flex-direction: row;
-	-webkit-box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.25);
-	box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.25);
-	height: 480px;
 
 	img {
-		//background: #a1aec0;
-		object-fit: contain;
-		//width: 40%;
-		width: 100%;
+		background: #a1aec0;
+		object-fit: cover;
+		width: 40%;
 		border-radius: 25px;
 		position: relative;
+	}
+
+	.imageLabel {
+		width: 38%;
+		position: absolute;
+		left: 5%;
+		top: 15px;
+		background-color: #a1aec0;
+		color: white;
+		z-index: 2;
+		border-radius: 5px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.details {
+		padding: 30px;
+		display: flex;
+		height: 100%;
+		flex: 1;
+		flex-direction: column;
+		justify-content: center;
+		align-self: flex-start;
+
+		.title {
+			display: flex;
+			flex: 1;
+			align-items: flex-end;
+		}
+
+		.content {
+			flex: 1;
+		}
+
+		.action {
+			align-self: end;
+			flex: 0 0 0;
+		}
 	}
 }
 
@@ -88,7 +145,8 @@ export default {
 
 	//@media (min-width: 1024px) {
 	.slick-slide {
-		padding: 10px;
+		padding-right: 10px;
+		padding-left: 10px;
 	}
 
 	//}
