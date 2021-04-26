517c30ebb506ec1c2ac3ee6ee8d9b844f0bb0b4b
Artem Martynyuk <scarg.dev@gmail.com>
2021-04-26 17:08:49 +0200

    feat(Newsletter): update newsletter with embed handling

--- components/sections/homepage/NewsletterSection.vue
+++ components/sections/homepage/NewsletterSection.vue
@@ -14,105 +14,30 @@
 			<p class="_text-center _margin-top-0">
 				{{ content.newsletterSection.subtitle }}
 			</p>
-			<i-form
-				v-model="form" class="_margin-bottom-1"
-				inline @submit.prevent="onSubmit"
-			>
-				<i-form-group>
-					<i-input
-						class="newsletter-input" :schema="form.contactEmail"
-						:placeholder="content.newsletterSection.emailPlaceholder"
-					/>
-					<cn-button
-						type="submit"
-						class="action-button"
-						:disabled="!verified || sending"
-						size="sm"
-						block
-					>
-						{{ sending ? 'Sending..' : content.newsletterSection.actionText }}
-					</cn-button>
-				</i-form-group>
-			</i-form>
-			<vue-recaptcha
-				size="sm"
-				ref="recaptcha"
-				:load-recaptcha-script="true"
-				:sitekey="sitekey"
-				@verify="onVerify"
-			/>
+			<div class="_display-flex _width-100 _justify-content-center _align-items-center">
+				<cn-button
+					type="submit"
+					class="action-button"
+					@click.native="newsletterModalOpen = true"
+				>
+					{{ content.newsletterSection.actionText }}
+				</cn-button>
+			</div>
 		</i-container>
+		<global-modal-newsletter v-model="newsletterModalOpen" />
 	</PageSection>
 </template>
 
 <script>
-import axios from 'axios';
-import { API_URL } from '@/services/api';
-import VueRecaptcha from 'vue-recaptcha';
 import homepage from '@/content/homepage';
 
 export default {
 	name: 'NewsletterSection',
-	components: {
-		VueRecaptcha,
-	},
 	data() {
 		return {
-			verified: false,
-			sending: false,
-			error: false,
-			sent: false,
-			sitekey: '6LeB9PUZAAAAAGJtwPEegkY2OeH1rVzHDwmdTguT',
-			form: this.$inkline.form({
-				contactEmail: {
-					validators: [
-						{ rule: 'required' },
-						{ rule: 'email' },
-					],
-				},
-			}),
+			newsletterModalOpen: false,
 			content: homepage,
 		};
-	},
-	methods: {
-		async onSubmit() {
-			if (!this.verified) {
-				return;
-			}
-
-			this.sending = true;
-
-			const email = this.form.contactEmail.value;
-			const data = {
-				email,
-			};
-
-			try {
-				await axios.post(`${ API_URL }/subscribe`, data, {
-					withCredentials: true,
-				});
-
-				this.sent = true;
-			}
-			catch (error) {
-				this.error = true;
-			}
-			finally {
-				this.sending = false;
-				this.verified = false;
-
-				setTimeout(() => {
-					this.sent = false;
-					this.error = false;
-					this.$emit('input', false);
-				}, 3000);
-			}
-		},
-		onVerify(response) {
-			if (response) {
-				this.verified = true;
-			}
-		},
 	},
 };
 </script>
@@ -124,37 +49,6 @@ export default {
 
 .container {
 	max-width: 500px;
-}
-
-::v-deep .form {
-	display: flex;
-	width: 100%;
-
-	.form-group {
-		width: 100%;
-		display: flex;
-		flex-wrap: wrap;
-
-		.form-group-error {
-			flex-basis: 100%;
-		}
-	}
-
-	.form-input-group {
-		flex: 1 1 0;
-
-		.form-input {
-			border: 3px solid #a1aec0;
-			border-radius: 5px;
-			margin-right: 5px;
-			height: $cn-button-height;
-
-			input {
-				padding: 3px;
-				border: 0;
-			}
-		}
-	}
 }
 
 .action-button {
