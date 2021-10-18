9a69cddf9600d089a1479bc1fa08b235faaabf59
Artemii Kravtsov <artyomkravtsov@gmail.com>
2021-10-18 12:45:07 +0300

    chore: open CI/CD form automatically if query param is present in URL

--- components/sections/common/CodenotaryCloudPageHeader.vue
+++ components/sections/common/CodenotaryCloudPageHeader.vue
@@ -124,6 +124,9 @@ export default {
 		if (typeof window !== 'undefined') {
 			this.calculateSecondaryBar();
 			window.addEventListener('resize', this.calculateSecondaryBar, { passive: true });
+
+			const { startDemoModalOpen = '0' } = this.$route.query;
+			this.startDemoModalOpen = startDemoModalOpen === '1';
 		}
 	},
 	methods: {
